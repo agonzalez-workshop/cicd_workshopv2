ee9d736ee0a9c555094ed2059b171950641a44a0
agonzalez-workshop <a.gonzalez.uc@gmail.com>
2024-08-27 14:52:06 -0400

    ECR

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -1,5 +1,14 @@
+import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import { AppCdkStack } from '../lib/app-cdk-stack';
 import { MyPipelineStack } from '../lib/pipeline-cdk-stack';
+import { EcrCdkStack } from '../lib/ecr-cdk-stack';
 
 const app = new cdk.App();
-new MyPipelineStack(app, 'MyPipelineStack');
+const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
+
+const pipelineCdkStack = new MyPipelineStack(app, 'MyPipelineStack', {
+    ecrRepository: ecrCdkStack.repository,
+  });
+
+//new MyPipelineStack(app, 'MyPipelineStack');

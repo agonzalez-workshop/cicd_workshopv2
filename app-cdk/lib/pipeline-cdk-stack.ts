5c52e729565bb9cebda6ed7b2d49734715e0fa16
agonzalez-workshop <a.gonzalez.uc@gmail.com>
2024-08-27 00:33:27 -0400

    five commit

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,12 +1,17 @@
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import * as cdk from 'aws-cdk-lib';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
 
+interface ConsumerProps extends StackProps {
+  ecrRepository: ecr.Repository,
+}
 export class MyPipelineStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: ConsumerProps) {
     super(scope, id, props);
 
     // Recupera el secreto de GitHub
@@ -19,37 +24,46 @@ export class MyPipelineStack extends cdk.Stack {
 
     // Define los artefactos
     const sourceOutput = new codepipeline.Artifact();
-    const buildOutput = new codepipeline.Artifact();
+    const unitTestOutput = new codepipeline.Artifact();
 
     // Define el pipeline
     const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
-      pipelineName: 'MyPipeline',
+      pipelineName: 'CICD_Pipeline',
+      crossAccountKeys: false,
+    });
+
+    const codeBuild = new codebuild.PipelineProject(this, 'CodeBuild', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+        privileged: true,
+        computeType: codebuild.ComputeType.LARGE,
+      },
     });
 
     // Agrega la etapa de origen con GitHub
     pipeline.addStage({
       stageName: 'Source',
       actions: [
-        new codepipeline_actions.CodeStarConnectionsSourceAction({
-          actionName: 'CheckOut',
+        new codepipeline_actions.GitHubSourceAction({
+          actionName: 'GitHub_Source',
           owner: 'agonzalez-workshop', // Nombre de la organización
-          repo: 'cicd_workshopv2',
-          output: sourceOutput,
+          repo: 'cicd_workshop',
           branch: 'main', // o la rama que prefieras
-          connectionArn: "arn:aws:secretsmanager:us-east-1:928159460500:secret:github/personal_access_token2-zKRk0G"
+          oauthToken: githubSecret.secretValue,
+          output: sourceOutput,
         }),
       ],
     });
 
-    // Agrega la etapa de construcción
+    // Agrega la etapa de construcción :)
     pipeline.addStage({
-      stageName: 'Build',
+      stageName: 'Code-Quality-Testing',
       actions: [
         new codepipeline_actions.CodeBuildAction({
-          actionName: 'Build',
-          project: buildProject,
+          actionName: 'Unit-Test',
+          project: codeBuild,
           input: sourceOutput,
-          outputs: [buildOutput],
+          outputs: [unitTestOutput],
         }),
       ],
     });
